93d62f8df41e583f79d43da81abd3520dcb9864b
Philip Johansson <philip_yeah@hotmail.com>
2020-12-21 18:40:25 +0100

    Remove prints

--- src/allergen_assessment.h
+++ src/allergen_assessment.h
@@ -70,79 +70,39 @@ namespace day21 {
 			}
 		}
 
-		for (auto it : allergenses) {
-			cout << it.first << endl;
-			for (string val : it.second) {
-				cout << val << ",";
-			}
-			cout << endl;
-		}
-
-		cout << "before locking " << endl;
 		//lock matches
 		c_type locked;
 		while (locked.size() != allergenses.size()) {
-			cout << "locked size " << locked.size() << endl;
-			
 			for (auto it : allergenses) {
-				cout << "handling " << it.first << endl;
-				cout << "values ";
-				for (string val : locked) {
-					cout << val << ",";
-				}
-				cout << endl;
 				if (allergenses[it.first].size() > 1) {
 					c_type tmp_c;
 					set_difference(allergenses[it.first].begin(), allergenses[it.first].end(), locked.begin(), locked.end(), inserter(tmp_c, tmp_c.begin()));
-					cout << allergenses[it.first].size() << endl;
-					cout << "left" << endl;
-					for (string s : tmp_c) {
-						cout << s << ",";
-					}
-					cout << endl;
 					allergenses[it.first] = tmp_c;
-					cout << allergenses[it.first].size() << endl;
 				}
 				if (allergenses[it.first].size() == 1) {
 					for (string val : it.second) {
 						locked.insert(val);
-						cout << "locked " << val << endl;
 					}
 				}
 			}
 		}
 
-		cout << "-------------------- left " << endl;
 		string sorted_allergens_ingredient_names = "";
 		for (auto it : allergenses) {
-			cout << it.first << endl;
 			for (string val : it.second) {
 				sorted_allergens_ingredient_names += val + ",";
-				cout << val << ",";
 			}
-			cout << endl;
 		}
+		//Remove last character
 		sorted_allergens_ingredient_names = sorted_allergens_ingredient_names.substr(0, sorted_allergens_ingredient_names.size() - 1);
-
-		cout << "----------------------------------------- locked " << endl;
-		for (string val : locked) {
-			cout << val << endl;
-		}
-		cout << endl;
 
 		int non_allergens_apperance_count = 0;
 		for (FoodInfo fi : all_foods) {
 			for (string val : fi.ingredients) {
-				cout << val << ",";
 				if (locked.find(val) == locked.end()) {
 					non_allergens_apperance_count++;
 				}
 			}
-			cout << " | ";
-			for (string val : fi.allergens) {
-				cout << val << ",";
-			}
-			cout << endl;
 		}
 
 		return { non_allergens_apperance_count,sorted_allergens_ingredient_names };
